e4ac595fca0a1e739e1a4b29cc8e207506e25cfd
cosminpintilie <cosmin_pintilie_96@yahoo.com>
2020-05-21 15:49:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from HeaderRecognition import recognitionTypeHeader
 from TableProcessing import detectTableWithCv2
 import cv2
 
-file = r'C:\Users\cosmin\PycharmProjects\invoice-extraction-tesseract\src\tabel.png'
+file = r'C:\Users\cosmin\PycharmProjects\invoice-extraction-tesseract\src\fact.jpg'
+cr1 = r'C:\Users\cosmin\PycharmProjects\invoice-extraction-tesseract\crop1.jpg'
+cr2 = r'C:\Users\cosmin\PycharmProjects\invoice-extraction-tesseract\crop2.jpg'
 
-header_final = r'C:\Faculty\Master1\Invoice\invoice-extraction-tesseract\final\fact.txt'
+header_final = r'C:\Users\cosmin\PycharmProjects\invoice-extraction-tesseract\final\fact.txt'
 
 if __name__ == '__main__':
     # extract header
-    
+
     type = recognitionTypeHeader.getType(file)
     print(type)
-    image1=cv2.imread(file)
+    image1 = cv2.imread(file)
     if type == "eon":
         crop1 = image1[0:805, 0:2000]
         cv2.imwrite("crop1.jpg", crop1)
@@ -28,10 +30,9 @@ if __name__ == '__main__':
         crop2 = image1[245:2000, 0:2000]
         cv2.imwrite("crop2.jpg", crop2)
 
+    header_data = recognitionTypeHeader.getContent(cr1, header_final, type)
+    recognitionTypeHeader.writeExcel(header_data, 'Output-Facturi.xlsx')
 
-    header_data = recognitionTypeHeader.getContent(crop1, header_final, type)
-    recognitionTypeHeader.writeExcel(header_data,'Output-Facturi.xlsx')
- 
     print("Nr Fact:", header_data[0])
     print("Seria Fact:", header_data[1])
     print("Data emiterii:", header_data[2])
@@ -45,8 +46,5 @@ if __name__ == '__main__':
     print("Cont Cump:", header_data[10])
     print("Banca Cump:", header_data[11])
 
-
-
     # tabel detect and recognize
-    detectTableWithCv2.detect(crop2)
-
+    detectTableWithCv2.detect(cr2)
